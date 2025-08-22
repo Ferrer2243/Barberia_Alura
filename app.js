ddee03b43eabe08df554acc4e034698eefd36ab4
Ferrer2243 <114366315+Ferrer2243@users.noreply.github.com>
2025-08-22 17:36:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.write("<h1>God Bless you</h1>");
+alert('Hello WOrld');
+prompt(" Me indicas un número:");
+
+let iitulo = document.querySelector(´h1´);
+titulo.innerHTM = ´Juego del Número Secreto´;
+
+let parrafo = document.querySelector (´h1´);
+parrafo.innerHTM = ´Indica un número del 1 al 10´;
